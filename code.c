4df564524aa797f47500ce7b196aaa8c96a64759
leedayun <52947672+leedayun@users.noreply.github.com>
2020-11-29 21:23:40 +0900

    Update code.c

--- code.c
+++ code.c
@@ -7,14 +7,13 @@ int main()
 
 	math = 80;
 	english = 100;
-	korean = 90;
-	sum = math + english + korean;
+	kor = 90;
+	sum = math + english + kor;
 
 	printf("This is a program that can print a report card.\n");
 
-	printf("Grades are as follow:   Korean : %d\nEnglish : %d\nMath : %d\n", korean, english, math);
+	printf("Grades are as follow:   Korean : %d\nEnglish : %d\nMath : %d\n", kor, english, math);
 	printf("Sum : %d\n", sum);
-	printf("Average: %f\n",(korean+english+math)/3.0);
 	printf("The End\n");
 
 	return 0;
