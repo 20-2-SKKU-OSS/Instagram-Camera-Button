dc38758c0da4afc11822a74efb635c2bdf0c5b5d
jihye0 <llimjihye@gmail.com>
2020-11-29 22:13:48 +0900

    add science

--- code.c
+++ code.c
@@ -2,21 +2,23 @@
 
 int main()
 {
-	int kor, english, math;
+	int kor, english, math, science;
 	int sum = 0;
 
 	math = 80;
 	english = 100;
 	kor = 90;
-	sum = math + english + kor;
+	science= 70;
+	sum = math + english + kor + science;
 
 	printf("This is a program that can print a report card.\n");
 
-	printf("Grades are as follow:   Korean : %d\nEnglish : %d\nMath : %d\n", kor, english, math);
+	printf("Grades are as follow: Science: %d\nKorean : %d\nEnglish : %d\nMath : %d\n", science, kor, english, math);
 	printf("Sum : %d\n", sum);
-	printf("Average: %f\n\n",(kor+english+math)/3.0);
+	printf("Average: %f\n\n",(kor+english+math+science)/4.0);
 
 	printf("The End\n");
+
 
 	return 0;
 }
