d26b72f53abe231c3226dc76048196e0cc245fdd
leedayun <kiwidy@naver.com>
2020-11-29 21:33:57 +0900

    add average score

--- code.c
+++ code.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-	int korean, english, math;
+	int kor, english, math;
 	int sum = 0;
 
 	math = 80;
 	english = 100;
-	korean = 90;
-	sum = math + english + korean;
+	kor = 90;
+	sum = math + english + kor;
 
 	printf("This is a program that can print a report card.\n");
 
-	printf("Grades are as follow:   Korean : %d\nEnglish : %d\nMath : %d\n", korean, english, math);
+	printf("Grades are as follow:   Korean : %d\nEnglish : %d\nMath : %d\n", kor, english, math);
 	printf("Sum : %d\n", sum);
-	printf("Average: %f\n\n",(korean+english+math)/3.0);
+	printf("Average: %f\n\n",(kor+english+math)/3.0);
 	printf("The End\n");
 
 	return 0;
