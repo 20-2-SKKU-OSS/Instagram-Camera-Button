5b8a9ecc201c0a3b7d56ca66825c8abbb433efb2
leedayun <kiwidy@naver.com>
2020-11-29 21:17:22 +0900

    change variable name to korean and add average score

--- code.c
+++ code.c
@@ -2,18 +2,19 @@
 
 int main()
 {
-	int kor, english, math;
+	int korean, english, math;
 	int sum = 0;
 
 	math = 80;
 	english = 100;
-	kor = 90;
-	sum = math + english + kor;
+	korean = 90;
+	sum = math + english + korean;
 
 	printf("This is a program that can print a report card.\n");
 
-	printf("Grades are as follow:   Korean : %d\nEnglish : %d\nMath : %d\n", kor, english, math);
+	printf("Grades are as follow:   Korean : %d\nEnglish : %d\nMath : %d\n", korean, english, math);
 	printf("Sum : %d\n", sum);
+	printf("Average: %f\n",(korean+english+math)/3.0);
 	printf("The End\n");
 
 	return 0;
